2eef8138adb5843cae02fb691931ef6f47ba1d11
RobottiFranco <franco.robotti0@gmail.com>
2024-05-11 01:30:23 -0300

    se agrega ejemplo de nodo trie

---
+++ src/main/java/ut5/main/Main.java
@@ -0,0 +1,32 @@
+package ut5.main;
+
+import java.util.LinkedList;
+
+import ut4.utils.ManejadorArchivosGenerico;
+import ut5.TDA.TArbolTrie;
+
+
+public class Main {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args){
+        TArbolTrie trie = new TArbolTrie();
+
+        String[] palabrasclave = ManejadorArchivosGenerico.leerArchivo("src\\main\\java\\ut5\\utils\\palabras1.txt");
+        for (String p : palabrasclave) {
+                trie.insertar(p);
+        }
+/*         trie.imprimir();
+ */
+
+
+        palabrasclave = ManejadorArchivosGenerico.leerArchivo("src\\main\\java\\ut5\\utils\\palabras.txt");
+        for (String p : palabrasclave) {
+                System.out.println(trie.buscar(p));
+        }
+/*         trie.imprimir();
+ */
+    }
+}
