4206fa0eb2bfe2bb45cc82c42b7d868ec9d8914b
RobottiFranco <franco.robotti0@gmail.com>
2024-06-11 14:05:40 -0300

    se agrega main floyd2

---
+++ src/main/java/ut7/main/floyd2.java
@@ -0,0 +1,25 @@
+package ut7.main;
+
+import ut7.TDA.TGrafoDirigido;
+import ut7.TDA.UtilGrafos;
+
+public class floyd2 {
+    public static void main(String[] args) {
+        TGrafoDirigido gd = (TGrafoDirigido) UtilGrafos.cargarGrafo("src\\main\\java\\ut7\\utils\\aeropuertos_1.txt", "src\\main\\java\\ut7\\utils\\conexiones_1.txt",
+                false, TGrafoDirigido.class);
+
+        Object[] etiquetasarray = gd.getEtiquetasOrdenado();
+
+        Double[][] matriz = UtilGrafos.obtenerMatrizCostos(gd.getVertices());
+        UtilGrafos.imprimirMatrizMejorado(matriz, gd.getVertices(), "Matriz");
+        Double[][] mfloyd = gd.floyd();
+        UtilGrafos.imprimirMatrizMejorado(mfloyd, gd.getVertices(), "Matriz luego de FLOYD");
+        for (int i = 0; i < etiquetasarray.length; i++) {
+            System.out.println("excentricidad de " + etiquetasarray[i] + " : "
+                    + gd.obtenerExcentricidad((Comparable) etiquetasarray[i]));
+        }
+        System.out.println();
+        System.out.println("Centro del grafo: " + gd.centroDelGrafo());
+
+    }
+}
